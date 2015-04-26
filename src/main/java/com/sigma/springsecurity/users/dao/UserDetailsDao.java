d63f8c22dd2679be4357a31e1799d72f63f5670e
tahirakhan <tahir.ali@vroozi.com>
2015-04-26 13:16:30 +0500

    Init

---
+++ src/main/java/com/sigma/springsecurity/users/dao/UserDetailsDao.java
@@ -0,0 +1,13 @@
+package com.sigma.springsecurity.users.dao;
+
+import com.sigma.springsecurity.users.model.UserAttempts;
+
+public interface UserDetailsDao {
+
+	void updateFailAttempts(String username);
+
+	void resetFailAttempts(String username);
+	
+	UserAttempts getUserAttempts(String username);
+
+}
